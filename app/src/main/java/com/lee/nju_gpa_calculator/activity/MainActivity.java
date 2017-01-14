6886e25aa31d5c3a886a8b9a837c8e440f370935
Leeski222 <244967114@qq.com>
2017-01-15 02:09:51 +0800

    添加AppData类保存App的数据 添加Constants类保存其他类中需要的常量数据 添加Score类作为单门学科分数的实现类，并用Parcelable接口实现序列化

--- app/src/main/java/com/lee/nju_gpa_calculator/activity/MainActivity.java
+++ app/src/main/java/com/lee/nju_gpa_calculator/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.lee.nju_gpa_calculator;
+package com.lee.nju_gpa_calculator.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.lee.nju_gpa_calculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/lee/nju_gpa_calculator/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.lee.nju_gpa_calculator.utils;
+
+/**
+ * Created by Guo on 2017/1/14.
+ */
+public class Constants {
+}
